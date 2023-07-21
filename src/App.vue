03ff228dbb2aa615d732ab19bb0ec0d682fbd443
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2023-07-22 01:29:47 +0200

    fixing layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <v-app>
-    <AppBar />
-    <Drawer />
+    <v-container>
+      <!-- AppBar -->
+      <v-row>
+        <v-col>
+          <AppBar />
+        </v-col>
+      </v-row>
+      <!-- Drawer and MainSection -->
+      <v-row>
+        <v-col>
+          <Drawer />
+        </v-col>
+        <v-col>
+          <MainSection />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
 import AppBar from "./views/Appbar.vue";
 import Drawer from "./views/Drawer.vue";
+import MainSection from "./views/MainSection.vue";
+
 export default {
   components: {
     AppBar,
     Drawer,
+    MainSection,
   },
 };
 </script>
